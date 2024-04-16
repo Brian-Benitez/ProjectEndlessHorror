507a981238fc9583606199feced5d31e8b0c7a05
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-04-15 22:19:54 -0400

    Moving objects to right position
    I am going spawn them in im just getting all the positions now then will spawn them in.

--- ProjectEndlessHorror/Assets/Code/LevelManager.cs
+++ ProjectEndlessHorror/Assets/Code/LevelManager.cs
@@ -9,13 +9,28 @@ public class LevelManager : MonoBehaviour
     public GameObject RedExitSignObject;
     public GameObject GreenExitSignObject;
     public GameObject ExitDoorObject;
+    public GameObject MainStairs;
+    public GameObject HallWay;
 
     [Header("Spawn Positions")]
-    public Transform SpawnPosition;
+    public GameObject StairsSpawnPosition;
+    public GameObject HallWaySpawnPostion;
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnNewChunk();
+            Debug.Log("i am clickjed");
+        }
+    }
+    /// <summary>
+    /// Spawns A new chunk of the level at the end of the level
+    /// </summary>
     public void SpawnNewChunk()
     {
-
+        MainStairs.gameObject.transform.position = StairsSpawnPosition.gameObject.transform.position;
+        HallWay.gameObject.transform.position = HallWaySpawnPostion.gameObject.transform.position;
     }
 
     public void SpawnPuzzleChunks()
