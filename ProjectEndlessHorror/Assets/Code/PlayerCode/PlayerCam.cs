23a45edeffbde533d849b73e307f13a8ca8f0f15
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-06-18 23:54:53 -0400

    Adding assets + making door function work
    Also, funny idea; Instead of spawning new rooms, just have objects of the room active soon as your done the level instead of spawning in a new object all the time.... seems easier and idk

--- ProjectEndlessHorror/Assets/Code/PlayerCode/PlayerCam.cs
+++ ProjectEndlessHorror/Assets/Code/PlayerCode/PlayerCam.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerCam : MonoBehaviour
