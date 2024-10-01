7036f0dcd112acd63db5a573dff19798f003eb27
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-09-30 21:43:27 -0400

    Level design

--- ProjectEndlessHorror/Assets/Code/Ghost Code/MonsterBehavior.cs
+++ ProjectEndlessHorror/Assets/Code/Ghost Code/MonsterBehavior.cs
@@ -5,39 +5,10 @@ using UnityEngine;
 public class MonsterBehavior : MonoBehaviour
 {
     public GameObject MonsterPrefab;
-    public Transform MonsterSpawnIn;
 
-    private void Start()
-    {
-        MonsterPrefab.SetActive(false);
-    }
-    /// <summary>
-    /// This function would talk about how the ghost moves. To clarify, to scare the player, not to follow the player in any movment.
-    /// </summary>
-    public void GhostMovement()
-    {
+    [Header("Transforms aka Spawnpoints")]
+    public List<Transform> MonsterSpawnList;
 
-    }
-    /// <summary>
-    /// This function talks about where the Ghost will spawn at during the players progression.
-    /// </summary>
-    public void GhostSpawnPoints()
-    {
-        // I will mark places the ghost can spawn in, places that the player cannot reach just yet or in front of them.
-    }
-    /// <summary>
-    /// Very self explaintory, the ghost will spawn behind the player and cast a shadow. It will be locked behind the player.
-    /// </summary>
-    public void GhostSpawnBehindPlayer()
-    {
 
-    }
-    /// <summary>
-    /// This function just resets the ghost pos before the player notices.
-    /// </summary>
-    public void ResetGhostPos()
-    {
-
-    }
 
 }
--- ProjectEndlessHorror/Assets/Code/IInteractableObjects/VOPhone.cs
+++ ProjectEndlessHorror/Assets/Code/IInteractableObjects/VOPhone.cs
@@ -7,10 +7,6 @@ public class VOPhone : MonoBehaviour, IInteractable
     public MonsterBehavior monsterBehavior;
     public void Interact()
     {
-        Puzzles puzzles = FindObjectOfType<Puzzles>();
-        //puzzles.ClickedOnVOPhone = true;
-        Debug.Log("monsterrrrrrrrr");
-        monsterBehavior.MonsterPrefab.transform.position = monsterBehavior.MonsterSpawnIn.transform.position;
-        monsterBehavior.MonsterPrefab.SetActive(true);
+       
     }
 }
