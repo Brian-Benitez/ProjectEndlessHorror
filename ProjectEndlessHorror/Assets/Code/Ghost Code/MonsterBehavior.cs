6dd4b2734a846f75d6fffa4b005382fa1bf0e0d5
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-10-02 21:27:32 -0400

    Added: Foundation of monster code
    still need more research on how I will do everything but this is a good start

--- ProjectEndlessHorror/Assets/Code/Ghost Code/MonsterBehavior.cs
+++ ProjectEndlessHorror/Assets/Code/Ghost Code/MonsterBehavior.cs
@@ -7,8 +7,30 @@ public class MonsterBehavior : MonoBehaviour
     public GameObject MonsterPrefab;
 
     [Header("Transforms aka Spawnpoints")]
-    public List<Transform> MonsterSpawnList;
+    public List<Transform> LevelTwoSpawnList;
+
+    [Header("Players pos")]
+    public Transform JumpScarePos;
 
 
+    private bool MonsterIsSeen = false;//somehow need to make this true
+    public void LevelOneMonsterBehavior()
+    {
+        MonsterPrefab.transform.position = LevelTwoSpawnList[0].transform.position;//First spawn the monster in the correct pos.
 
+        if(MonsterIsSeen)
+        {
+            //Somehow when seen, disapear from the scene
+            //randomly pick a spot to spawn in watching the player.
+            int spawnPoint = Random.Range(0, LevelTwoSpawnList.Count);
+            MonsterPrefab.transform.position = LevelTwoSpawnList[spawnPoint].transform.position;
+            //need to somehow check if the player sees the monster then do something
+        }
+    }
+
+    public void MonstersJumpScarePosition()
+    {
+        MonsterPrefab.transform.position = JumpScarePos.transform.position;
+        //Play jump scare animation here.
+    }
 }
