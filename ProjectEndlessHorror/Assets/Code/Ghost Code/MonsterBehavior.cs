66f5a296b042ea2a2d62756ae705f94a2d0168b0
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-10-03 20:00:42 -0400

    Code: Monster can now go to end points
    Monster can go to points then despawns. Next is making it happen when player hits a collider

--- ProjectEndlessHorror/Assets/Code/Ghost Code/MonsterBehavior.cs
+++ ProjectEndlessHorror/Assets/Code/Ghost Code/MonsterBehavior.cs
@@ -4,33 +4,32 @@ using UnityEngine;
 
 public class MonsterBehavior : MonoBehaviour
 {
-    public GameObject MonsterPrefab;
+    [Header("Monsters Info")]
+    [SerializeField] private float _speed = 1.5f;//Monsters speed to points or to the player
 
-    [Header("Transforms aka Spawnpoints")]
-    public List<Transform> LevelTwoSpawnList;
-
-    [Header("Players pos")]
+    [Header("Positions")]
     public Transform JumpScarePos;
+    public GameObject EndPoint;
 
-
-    private bool MonsterIsSeen = false;//somehow need to make this true
-    public void LevelOneMonsterBehavior()
+    private void Update()
     {
-        MonsterPrefab.transform.position = LevelTwoSpawnList[0].transform.position;//First spawn the monster in the correct pos.
+        MoveMonster();
+    }
 
-        if(MonsterIsSeen)
+    public void MoveMonster()
+    {
+        if(this.transform.position == EndPoint.transform.position)
         {
-            //Somehow when seen, disapear from the scene
-            //randomly pick a spot to spawn in watching the player.
-            int spawnPoint = Random.Range(0, LevelTwoSpawnList.Count);
-            MonsterPrefab.transform.position = LevelTwoSpawnList[spawnPoint].transform.position;
-            //need to somehow check if the player sees the monster then do something
+            this.transform.gameObject.SetActive(false);
+            return;
         }
+        else
+            transform.position = Vector3.MoveTowards(transform.position, EndPoint.transform.position, _speed * Time.deltaTime);
     }
 
     public void MonstersJumpScarePosition()
     {
-        MonsterPrefab.transform.position = JumpScarePos.transform.position;
+        this.transform.position = JumpScarePos.transform.position;
         //Play jump scare animation here.
     }
 }
