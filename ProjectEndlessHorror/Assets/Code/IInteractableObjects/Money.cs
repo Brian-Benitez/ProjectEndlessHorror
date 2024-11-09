dcc625b610e1459b8f93ea76db0dd306e946648e
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2024-11-09 15:07:25 -0500

    Fix + Adjustments: money placement and adjust door text text
    I also made it so I doesn't give a error when you finish the level. Need to add post processing now.

--- ProjectEndlessHorror/Assets/Code/IInteractableObjects/Money.cs
+++ ProjectEndlessHorror/Assets/Code/IInteractableObjects/Money.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class Money : MonoBehaviour, IInteractable
 {
     public PlayerInventory PlayerInventoryRef;
+    public LevelManager LevelManagerRef;
     public void Interact()
     {
         PlayerInventoryRef.PlayersInventoryList.Add(transform.gameObject);
         Debug.Log("added key to inventory");
+        LevelManagerRef.RotateSideDoor();
+        GameMain.instance.PlayingScaryEventDelegate();//This is the event playing for when its level four and the scary event will play.
         transform.gameObject.SetActive(false);//set 
     }
 }
--- ProjectEndlessHorror/Assets/Code/IInteractableObjects/SideDoor.cs
+++ ProjectEndlessHorror/Assets/Code/IInteractableObjects/SideDoor.cs
@@ -3,8 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SideDoor : MonoBehaviour, IInteractable
+public class SideDoor : MonoBehaviour//, IInteractable
 {
+    /*
     public PlayerInventory PlayerInventoryRef;
     public LevelManager LevelManagerRef;
     public MonsterBehavior MonsterBehaviorRef;
@@ -17,9 +18,10 @@ public class SideDoor : MonoBehaviour, IInteractable
         else
             Debug.Log("player does not have a second door key");
 
-        GameMain.instance.PlayingScaryEventDelegate();//This is the event playing for when its level four and the scary event will play.
+        
 
         if(LevelManagerRef.LevelIndex == 4)//this is for the "deletion" of the key.
             transform.gameObject.SetActive(false);
     }
+    */
 }
