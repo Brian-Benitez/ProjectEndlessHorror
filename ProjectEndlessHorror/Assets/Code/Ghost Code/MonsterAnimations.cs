50a2a339ad188ccd4a62f9b6d88e1fe7636d4cc8
SalvadoranTrash <107580111+Brian-Benitez@users.noreply.github.com>
2025-01-12 16:32:19 -0500

    Made: Monster script and added more animations to monster

--- ProjectEndlessHorror/Assets/Code/Ghost Code/MonsterAnimations.cs
+++ ProjectEndlessHorror/Assets/Code/Ghost Code/MonsterAnimations.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class MonsterAnimations : MonoBehaviour
 {
+    [Header("Animator")]
     public Animator MonsterAnimator;
 
-    private void Start()
+    [Header("Scripts")]
+    public LevelManager LevelManagerRef;
+
+    public void SetAnimationForMonster()
     {
-        
+        SetAllBoolsToFalse();
+
+        switch (LevelManagerRef.LevelIndex)
+        {
+            case 1://Walking encounter
+                MonsterAnimator.SetBool("IsWalking", true);
+                break;
+
+            default:
+                break;
+                
+        }
+    }
+    /// <summary>
+    /// Set all bools from the animator to false.
+    /// </summary>
+    private void SetAllBoolsToFalse()
+    {
+        MonsterAnimator.SetBool("IsWalking", false);
+        MonsterAnimator.SetBool("IsKneeling", false);
+        MonsterAnimator.SetBool("IsIdle", false);
+        MonsterAnimator.SetBool("IsRunning", false);
     }
 }
